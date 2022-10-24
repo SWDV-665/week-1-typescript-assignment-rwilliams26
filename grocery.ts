188eeda917ca33df9fb64388e538ed8e4f4b5b86
rwilliams26 <rwilliams26@live.maryville.edu>
2022-10-23 19:13:02 -0500

    Week 1 Assignment

--- grocery.ts
+++ grocery.ts
@@ -1,11 +1,9 @@
-// create a class
+
 class Grocery {
-    // declare properties and corresponding data type
     name: string;
     quantity: number;
     price: number;
 
-    // add a constructor
     constructor(n: string, q: number, p: number){
         this.name = n;
         this.quantity = q;
@@ -14,7 +12,6 @@ class Grocery {
 }
 
 // create a list of grocery items
-// new keyword is used to inititalise objects
 let list_of_items = [
     new Grocery("candy", 3, 1),
     new Grocery("soda", 2, 3),
@@ -24,8 +21,6 @@ let list_of_items = [
 // access the html element with id app
 const ele = document.getElementById("app");
 
-// create a <p> element for each item in the grocery list and
-// append it to the html page
 list_of_items.forEach(e => {
     const p = document.createElement("p");
     p.textContent = `${e.name} ${e.quantity} -> $${e.price}`;
